a4cefd1d006f38b71d080df0276521d20ae657ee
yujin <uj0791navercom@skuniv.ac.kr>
2024-09-14 00:46:43 +0900

    [FEAT] 회원가입 서비스 작성

---
+++ src/main/java/backend/budget/auth/controller/AuthController.java
@@ -0,0 +1,29 @@
+package backend.budget.auth.controller;
+
+import backend.budget.auth.dto.RegisterRequest;
+import backend.budget.auth.service.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody @Valid RegisterRequest request){
+        userService.register(request);
+    }
+}
